8f6bd5b336e79d3d21bc4f3c46a1ac8da51064fc
João Gabriel <main.joaogabrielsg@gmail.com>
2022-04-11 21:31:57 -0300

    feat: add import categories via file

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
+
+const upload = multer({ dest: './temp' });
 
 const categoriesRoutes = Router();
 
@@ -11,6 +15,10 @@ categoriesRoutes.get('/', (req, res) => {
 
 categoriesRoutes.post('/', (req, res) => {
   return createCategoryController.handle(req, res);
+});
+
+categoriesRoutes.post('/import', upload.single('file'), (req, res) => {
+  return importCategoryController.handle(req, res);
 });
 
 export { categoriesRoutes };
