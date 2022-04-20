0daba10272b588556c01d6a177a6a838ee8bafb3
João Gabriel <main.joaogabrielsg@gmail.com>
2022-04-19 22:48:23 -0300

    feat: create user token

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,55 @@
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import { UsersRepository } from '../../repositories/implementations/UsersRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  token: string;
+  user: {
+    name: string;
+    email: string;
+  };
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: UsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error('Email or password incorrect');
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error('Email or password incorrect');
+    }
+
+    const token = sign({}, '0bae5bc35002076168ddeaf7897022d7', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    };
+  }
+}
+
+export { AuthenticateUserUseCase };
