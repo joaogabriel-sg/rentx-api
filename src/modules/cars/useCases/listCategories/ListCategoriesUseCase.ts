ea935d7301fb63b44a960b3c6fdd440a568502bf
João Gabriel <main.joaogabrielsg@gmail.com>
2022-04-22 23:18:49 -0300

    refactor: add path mapping
    - App path mapping;
    - Fix imports and ESLint rules.

--- src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -1,6 +1,6 @@
 import { inject, injectable } from 'tsyringe';
 
-import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
 
 @injectable()
 class ListCategoriesUseCase {
--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,12 @@ import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
-import { AppError } from './errors/AppError';
+import { AppError } from '@errors/AppError';
+
 import { router } from './routes';
 import swaggerFile from './swagger.json';
 import './database';
-import './shared/container';
+import '@shared/container';
 
 const app = express();
 
