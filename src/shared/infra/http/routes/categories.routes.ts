0d48915ac15c9d1c2829de9fed9f19f331f98246
João Gabriel <main.joaogabrielsg@gmail.com>
2022-04-25 22:17:24 -0300

    feat: add ensure admin middleware

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -5,6 +5,9 @@ import { CreateCategoryController } from '@modules/cars/useCases/createCategory/
 import { ImportCategoryController } from '@modules/cars/useCases/importCategory/ImportCategoryController';
 import { ListCategoriesController } from '@modules/cars/useCases/listCategories/ListCategoriesController';
 
+import { ensureAdmin } from '../middlewares/ensureAdmin';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+
 const upload = multer({ dest: './temp' });
 
 const categoriesRoutes = Router();
@@ -15,11 +18,18 @@ const listCategoriesController = new ListCategoriesController();
 
 categoriesRoutes.get('/', listCategoriesController.handle);
 
-categoriesRoutes.post('/', createCategoryController.handle);
+categoriesRoutes.post(
+  '/',
+  ensureAuthenticated,
+  ensureAdmin,
+  createCategoryController.handle
+);
 
 categoriesRoutes.post(
   '/import',
   upload.single('file'),
+  ensureAuthenticated,
+  ensureAdmin,
   importCategoryController.handle
 );
 
